e976a181ac9eee3d686c182e50b707274ef678f5
Sunita <sunitaanand1230@gmail.com>
2026-06-19 23:19:06 +0530

    commit autowaiting

---
+++ tests/autowaiting_and_timeouts.spec.ts
@@ -0,0 +1,17 @@
+//! Autowaiting 
+
+//? 1. Autowaiting terms itself says that it is waiting for something.
+
+//? 2. When we put assertion to the script, they will wait for some time.
+
+//? 3. Autowait mechanism is a default feature in playwright.
+
+//? 4. We no need to take care of waiting statements.
+
+//? 5. Playwright performs a range of actionability checks on the 
+//? elements before making actions to ensure these actions behave as expected.
+
+//? 6. If the actionability checks are failed , then it will return a time outerror.
+
+//? 7. Default time out provided by playwright is 30 seconds.
+
